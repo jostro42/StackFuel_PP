4ce474dfbc4f706643f64d4bc9eaf31427118cde
jostro42 <95870449+jostro42@users.noreply.github.com>
2026-09-17 16:38:46 +0200

    created platform_slugs.json, containing the platform-slug mappings for easier access later

--- src/core/config.py
+++ src/core/config.py
@@ -9,6 +9,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[2]
 DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
+MAPPINGS_DIR = DATA_DIR / "mappings"
 
 DATA_RAW_PATH = RAW_DATA_DIR / "dataset_metacritic_scraper_2025-02-15.csv"
 DATA_FORMATTED_PATH = PROCESSED_DATA_DIR / "dataset_metacritic_scraper_reformatted.csv"
